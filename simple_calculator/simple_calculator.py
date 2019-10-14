5f7400895d2acaaf7dfb18c41c337137ec55fc4d
ezems123 <ezems123@gmail.com>
2019-10-14 17:26:12 +0100

    refactored code

--- simple_calculator/simple_calculator.py
+++ simple_calculator/simple_calculator.py
@@ -7,25 +7,30 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="calculator")
-    parser.add_argument("-operation", type=str,
-                        help="Which operation do you want to perform? Choose add, sub, mul or div?")
-    parser.add_argument("x", type=int, help="You need to enter a number")
-    parser.add_argument(
-        "y", type=int, help="You need to enter a second number")
+    parser.add_argument('integers', metavar='N', type=int, nargs=2,
+                        help='an integer for the accumulator')
+    parser.add_argument('--add', action='store_true',
+                        help="Adds a set of numbers")
+    parser.add_argument('--sub', action='store_true',
+                        help="Subtracts a set of numbers")
+    parser.add_argument('--multiply', action='store_true',
+                        help="Multiplies a set of numbers")
+    parser.add_argument('--divide', action='store_true',
+                        help="Divides a set of numbers")
 
     args = parser.parse_args()
     print(calc(args))
 
 
 def calc(args):
-    if args.operation == "add":
-        return "{} + {} = {}".format(args.x, args.y, args.x + args.y)
-    elif args.operation == "sub":
-        return "{} - {} = {}".format(args.x, args.y, args.x - args.y)
-    elif args.operation == "mul":
-        return "{} * {} = {}".format(args.x, args.y, args.x * args.y)
-    elif args.operation == "div":
-        return "{} / {} = {}".format(args.x, args.y, args.x / args.y)
+    if args.add:
+        return sum(args.integers)
+    if args.sub:
+        return args.integers[0]-args.integers[1]
+    if args.multiply:
+        return args.integers[0]*args.integers[1]
+    if args.divide:
+        return args.integers[0]/args.integers[1]
     else:
         return "You have chosen an invalid opearation"
 
